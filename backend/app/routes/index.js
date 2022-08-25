6910c577912cb4e6384fb167e7cc0e149fede7cd
james <independente30108@gmail.com>
2022-08-25 23:02:23 +0700

    25-8-65 // 1

--- backend/app/routes/index.js
+++ backend/app/routes/index.js
@@ -1,10 +1,11 @@
-const express   = require("express") // import Express เข้ามาทำงาน
-const router    = express.Router() // Router จะทำงานเกี่ยวกับระบบรับส่งข้อมูลทั้งหมด
-
+const express       = require("express") // import Express เข้ามาทำงาน
+const authorization = require('../config/authorize')
+const router        = express.Router() // Router จะทำงานเกี่ยวกับระบบรับส่งข้อมูลทั้งหมด
 
 //router.use("/api/create_tale", require("./router_create_tale"))
-router.use("/api/product", require("./router_product"))
-router.use("/api/product_type", require("./router_product_type"))
-router.use("/api/product_attribute", require("./router_product_attribute"))
+router.use("/login", require('./login'))
+router.use("/api/product", authorization, require("./router_product"))
+router.use("/api/product_type", authorization, require("./router_product_type"))
+router.use("/api/product_attribute", authorization, require("./router_product_attribute"))
 
 module.exports = router // ทำการ export 
