170f53e496f0091b5fdd2ca7a805c6356783f5eb
james <independente30108@gmail.com>
2022-09-04 12:17:32 +0700

    4-9-65

--- frontend/src/components/product/index.vue
+++ frontend/src/components/product/index.vue
@@ -63,6 +63,7 @@
             <router-link to="/product_manage" class="btn btn-success text-white mb-3 mx-1">เพิ่มสินค้า</router-link>
         
             <!-- Button trigger modal -->
+            <!--
             <button
                 type="button"
                 class="btn btn-outline-primary mb-3 mx-1"
@@ -72,8 +73,8 @@
                 ค้นหา
             </button>
             <button type="button" class="btn btn-outline-secondary mb-3 mx-1" @click="reset">คืนค่า</button>
+            -->
 
-            
         </div>
         <div class="card">
         <h5 class="card-header">รายการ</h5>
@@ -104,7 +105,7 @@
                         >
                         <img 
                             v-else="item.product_image_cover != '' "
-                            src="../../assets/img/avatars/1.png" 
+                            src="/assets/img/products/example_product.png"
                             class="d-block rounded"
                             height="100"
                             width="100"
@@ -112,11 +113,11 @@
                     </td>
                     <td><button @click="get_one(item.product_id)" class="bg-transparent border-0 text-primary">{{ item.product_name }}</button></td>
                     <td>{{ item.product_code }}</td>
-                    <td>{{ item.product_code }}</td>
-                    <td>{{ item.product_code }}</td>
-                    <td>{{ item.product_code }}</td>
+                    <td>demo</td>
+                    <td>demo</td>
+                    <td>demo</td>
                     <td>
-                        <button @click="delete_one(item.product_id)" class="me-1 bg-transparent border-0"><i class="bx bx-trash me-1 font-22 text-primary"></i></button> 
+                        <button @click="delete_one(item.product_id)" class="me-1 bg-transparent border-0"><i class="bx bx-trash me-1 font-22 text-danger"></i></button> 
                     </td>
                 </tr>
             </tbody>
